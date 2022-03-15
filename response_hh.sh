f87952bd76b1531cf1147557b9ec18936196e094
Vasiliy <vasiliy.6393@gmail.com>
2022-03-16 04:30:07 +0500

    Update response_hh.sh

--- response_hh.sh
+++ response_hh.sh
@@ -3,12 +3,16 @@
 #  AUTHOR: Vasiliy Pogoreliy, vasiliy@pogoreliy.ru 
 PATH="/my_bin:$PATH";
 
+# Паралельно начал делать веб-интерфейс для скрипта
+# Исходники веб-интерфейса скорее всего выложены не будут
+# Но будет оставлена ссылка в этом исходнике
+
 export SED="$(which sed)";
 for i in tr jq ps cat awk grep head tail date sleep kill curl sleep; do
     eval $(echo "export $(echo "$i" | $SED 's/.*/\U&/g')=\$(which $i);");
 done
 
-if [[ ! -z $1 ]]; then USER="$1"; else echo "Field \"user\" is empty. Exiting."; exit; fi
+if [[ ! -z $1 ]]; then export USER="$1"; else echo "Field \"user\" is empty. Exiting."; exit; fi
 
 export JQc="$(which jq) --compact-output";
 export TELEGRAM_SEND="$(which telegram_send.sh)"
@@ -66,61 +70,54 @@ function WAIT(){
     done
 }
 
-function main(){
+function SEND_RESUME(){
+    # Функция отправки отклика
     RESUME_ID="$1";
-    MSG="$2";
-    n="$3";
-    spec_1="$(echo "$MSG" | $SED 's/.*рассмотреть моё резюме "\([^"]\+\)"\./\1/')";
-    spec_2="$(echo "$spec_1" | $SED 's/\-/ /')";
-    spec="$(for s in $spec_2; do
-        echo -en "specialization=";
-        s="$(echo -en "$s" | $SED 's/^.//')";
-        $CURL -H "$HEADERS_AUTH" -s -X GET "$URL/specializations" | jq |
-            $GREP -P -B1 "$s" | $GREP -P '"id":' | $SED 's/.*"id": "\([0-9\.]\+\)",/\1/' |
-            $TR "\n" "&";
-    done)"
-    $CURL -H "$HEADERS_AUTH" -s -X GET "$URL/vacancies?${spec}area=3&per_page=10&page=$n&top_lat=56.918873&bottom_lat=56.880062&left_lng=60.562901&right_lng=60.691647" |
-    $JQc '.items[] | {id, address}' | $AWK -F\" '{print $4" "$14}' |
-    while read vacancy_id_address; do
-        vacancy_address="$($SED 's/^[0-9]\+ \?//' <<< "$vacancy_id_address")";
-        vacancy_address="$($SED 's/ \?[Уу]лица \?\| \?[Пп]ереулок \?\| \?[Тт]упик \?\| \?[Пп]роспект \?//' <<< "$vacancy_address")";
-        vacancy_address="$($SED 's/[а-яА-ЯёЁ]\+ район//' <<< "$vacancy_address")";
-        vacancy_address="$($SED 's/микрорайон [а-яА-ЯёЁ]\+//' <<< "$vacancy_address")";
-        vacancy_address="$($SED 's/, \?//g' <<< "$vacancy_address")";
-        vacancy_id="$($AWK '{print $1}' <<< "$vacancy_id_address")";
-        # Если в файле содержится $vacancy_id
-        # значит резюме уже отправляли, не будем зря отвлекать людей от работы
-        if ! $GREP -Pq "$vacancy_id" <<< "$LOG"; then
-            if [[ "a$vacancy_address" != "a" ]]; then
-                if $GREP -Piq "$vacancy_address" "/var/log/hh/hh_streets.lst"; then
-                RES_RESPONSE="$($CURL -H "$HEADERS_AUTH" -H "$HEADERS_CONTENT_TYPE" \
-                    -s -X POST "$URL/negotiations" -F "vacancy_id=$vacancy_id" \
-                                                   -F "resume_id=$RESUME_ID" -F "message=$MSG")";
-                    if echo "$RES_RESPONSE" | $GREP -Pq "$LIMIT_EXCEEDED"; then
-                        # Если дневной лимит превышен
-                        # Ждём 08:00 следующего дня.
-                        # Резюме отправлено не будет, но мне это не критично -
-                        # Одним больше, одним меньше - какая разница если дневной
-                        # лимит пара сотен =)
-                        # Тем более оно всё равно будет отправлено со следующего
-                        # прогона цикла.
-                        echo "$($DATE): $vacancy_id wait next day (limit_exceeded)";
-                        echo "$($DATE): $vacancy_id wait next day (limit_exceeded)" >> $LOG;
-                        WAIT "08:00 next day";
-                    elif echo "$RES_RESPONSE" | $GREP -Pq "$ALREADY_APPLIED"; then
-                        echo "$($DATE): $vacancy_id not send (already_applied)";
-                        echo "$($DATE): $vacancy_id not send (already_applied)" >> $LOG;
-                        continue;
-                    elif [[ "a$RES_RESPONSE" != "a" ]]; then
-                        echo "$RES_RESPONSE" >> $LOG;
-                    else
-                        echo "$RES_RESPONSE";
-                        echo "$($DATE): vacancy_id=$vacancy_id resume_id=$RESUME_ID message=$MSG" >> $LOG;
-                    fi
+    vacancy_id_address="$2";
+    vacancy_address="$($SED 's/^[0-9]\+ \?//' <<< "$vacancy_id_address")";
+    vacancy_address="$($SED 's/ \?[Уу]лица \?\| \?[Пп]ереулок \?\| \?[Тт]упик \?\| \?[Пп]роспект \?//' <<< "$vacancy_address")";
+    vacancy_address="$($SED 's/[а-яА-ЯёЁ]\+ район//' <<< "$vacancy_address")";
+    vacancy_address="$($SED 's/микрорайон [а-яА-ЯёЁ]\+//' <<< "$vacancy_address")";
+    vacancy_address="$($SED 's/, \?//g' <<< "$vacancy_address")";
+    vacancy_id="$($AWK '{print $1}' <<< "$vacancy_id_address")";
+    # Если в файле содержится $vacancy_id
+    # значит резюме уже отправляли, не будем зря отвлекать людей от работы
+    if ! $GREP -Pq "$vacancy_id" <<< "$LOG"; then
+        # Проверка адреса по списку улиц, в будущем скорее всего уберу
+        if [[ "a$vacancy_address" != "a" ]]; then # Неуказанные адреса игнорим
+            if $GREP -Piq "$vacancy_address" "/var/log/hh/hh_streets.lst"; then
+            RES_RESPONSE="$($CURL -H "$HEADERS_AUTH" -H "$HEADERS_CONTENT_TYPE" \
+                -s -X POST "$URL/negotiations" -F "vacancy_id=$vacancy_id" \
+                                               -F "resume_id=$RESUME_ID" -F "message=$MSG")";
+                if echo "$RES_RESPONSE" | $GREP -Pq "$LIMIT_EXCEEDED"; then
+                    # Если дневной лимит превышен
+                    # Ждём 08:00 следующего дня.
+                    # Резюме отправлено не будет, но мне это не критично -
+                    # Одним больше, одним меньше - какая разница если дневной
+                    # лимит пара сотен =)
+                    # Тем более оно всё равно будет отправлено со следующего
+                    # прогона цикла.
+                    echo "$($DATE): $vacancy_id wait next day (limit_exceeded)";
+                    echo "$($DATE): $vacancy_id wait next day (limit_exceeded)" >> $LOG;
+                    WAIT "08:00 next day";
+                elif echo "$RES_RESPONSE" | $GREP -Pq "$ALREADY_APPLIED"; then
+                    echo "$($DATE): $vacancy_id not send (already_applied)";
+                    # echo "$($DATE): $vacancy_id not send (already_applied)" >> $LOG;
+                    continue;
+                elif [[ "a$RES_RESPONSE" != "a" ]]; then
+                    echo "$RES_RESPONSE" >> $LOG;
+                else
+                    echo "$RES_RESPONSE";
+                    echo "$($DATE): vacancy_id=$vacancy_id resume_id=$RESUME_ID message=$MSG" >> $LOG;
                 fi
             fi
         fi
-    done
+    fi
+}
+
+function FATAL_ERROR(){
+    $TELEGRAM_SEND "$0: ошибка, скрипт остановлен" > /dev/null 2>&1;
+    exit;
 }
 
 resume_list="$($CURL -H "$HEADERS_AUTH" -s -X GET "$URL/resumes/mine" |
@@ -130,21 +127,29 @@ resume_list="$($CURL -H "$HEADERS_AUTH" -s -X GET "$URL/resumes/mine" |
                    $SED 's/^\([^ ]\+\) "\([^"]\+\)".*/\1 \2/')";
 
 cycle=0;
+repeat="false";
 while true; do
+    max_pages="$2";
     if [[ "$cycle" -ge "200" ]]; then
-        echo "$($DATE): sending stoped" >> $LOG;
-        break;
+        cycle="0";
+        repeat="true";
+        sleep 5m;
+        resume_list="$($CURL -H "$HEADERS_AUTH" -s -X GET "$URL/resumes/mine" |
+                           $JQc '.items[] | {id, title, access}' |
+                           $AWK -F\" '{print $4" \""$8"\" "$20}' | 
+                           $GREP -Pv 'выбранным компаниям|по прямой ссылке|никому' |
+                           $SED 's/^\([^ ]\+\) "\([^"]\+\)".*/\1 \2/')";
     fi
-    if [[ ! -z $2 ]] && [[ "$cycle" == "$2" ]]; then exit; fi
+    if [[ ! -z $max_pages ]] && [[ "$cycle" -gt "$max_pages" ]]; then break; fi
     now="$($DATE +%-H)";
-    if [[ "$now" -gt "20" ]]; then
+    if [[ "$now" -ge "20" ]]; then
         echo "Wait next day; PID=$$;" >> $LOG;
         WAIT "08:00 next day"; # Не беспокоим людей в вечернее время
-    elif [[ "$now" -lt "4" ]]; then
+    elif [[ "$now" -lt "8" ]]; then
         echo "Wait 08:00; PID=$$;" >> $LOG;
         WAIT "08:00"; # Не беспокоим ночью, дождёмся начала рабочего дня (08:00)
     fi
-    if [[ "$cycle" -eq "0" ]]; then
+    if [[ "$cycle" -eq "0" ]] && [[ "$repeat" != "true" ]]; then
         echo "Started: DATE=$(date +"%d.%m.%Y | %H:%M:%S"); PID=$$;" >> $LOG;
         $TELEGRAM_SEND "$0: начало рассылки резюме" > /dev/null 2>&1;
     fi
@@ -153,12 +158,32 @@ while true; do
         rid="$(echo "$r" | $AWK '{print $1}')";
         rname="$(echo "$r" | $SED 's/^[0-9a-zA-Z]\+ //')";
         resume=("$rid" "$MSG \"$rname\"." );
-        main "${resume[0]}" "${resume[1]}" "$cycle" || FATAL_ERROR="true"; $SLEEP 5;
+        RESUME_ID="${resume[0]}";
+        MSG="${resume[1]}";
+        spec_1="$(echo "$MSG" | $SED 's/.*рассмотреть моё резюме "\([^"]\+\)"\./\1/')";
+        spec_2="$(echo "$spec_1" | $SED 's/\-/ /')";
+        spec="$(for s in $spec_2; do
+            echo -en "specialization=";
+            s="$(echo -en "$s" | $SED 's/^.//')";
+            $CURL -H "$HEADERS_AUTH" -s -X GET "$URL/specializations" | jq |
+                $GREP -P -B1 "$s" | $GREP -P '"id":' | $SED 's/.*"id": "\([0-9\.]\+\)",/\1/' |
+                $TR "\n" "&";
+        done)";
+        per_page="per_page=10";
+        page="page=$cycle";
+        metro="metro=48.261&metro=48.262&metro=48.263";
+        premium="premium=true";
+        $CURL -H "$HEADERS_AUTH" -s -X GET "$URL/vacancies?$spec&$per_page&$page&$metro&$premium" |
+            $JQc '.items[] | {id, address}' | $AWK -F\" '{print $4" "$14}' |
+                while read vacancy_id_address; do
+                    if grep -Piq 'грузчик' <<< "$vacancy_id_address"; then
+                        if [[ "$USER" == "vasiliy" ]]; then
+                            SEND_RESUME "$RESUME_ID" "$vacancy_id_address" || FATAL_ERROR;
+                        fi
+                    else
+                        SEND_RESUME "$RESUME_ID" "$vacancy_id_address" | FATAL_ERROR;
+                    fi
+                done
     done
-
-    if [[ ! -z $FATAL_ERROR ]]; then
-        $TELEGRAM_SEND "$0: ошибка, скрипт остановлен" > /dev/null 2>&1;
-        exit;
-    fi
     cycle=$(($cycle+1));
 done
